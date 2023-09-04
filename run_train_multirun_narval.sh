4e8313da346c57dc5cfe3c5ab21c81c6a3568cb5
amin <amansouri3476@gmail.com>
2023-09-04 17:20:25 -0400

    narval minor updates

--- run_train_multirun_narval.sh
+++ run_train_multirun_narval.sh
@@ -62,6 +62,33 @@ export LD_PRELOAD=/home/aminm/mechanism-based-disentanglement/disentanglement_by
 # python run_training.py ckpt_path=null hydra/launcher=submitit_slurm_narval_cpu trainer.gpus=0 model/optimizer=adam model.optimizer.lr=0.01,0.001 model=mixing_synthetic datamodule=mixing ~callbacks.visualization_callback model.penalty_weight=1.0,0.1 datamodule.dataset.z_dim=20 model.wait_steps=0,1000 model.linear_steps=1,2000 logger.wandb.tags=["mila","4-domain","fixed"] ~callbacks.early_stopping --multirun
 
 # ------------------------------------------------------------------------------------- #
+# ---------- Sweep No dimension mismatch, min-max penalty, no hinge loss -------------- #
+# cpu, each sweep contains 2x3x5x3x2x2=360 runs
+# num_domains=2
+# python run_training.py ckpt_path=null trainer.gpus=0 model/optimizer=adam,adamw model.optimizer.lr=0.05,0.01,0.001 model/scheduler_config=reduce_on_plateau model=mixing_synthetic datamodule=mixing datamodule.dataset.num_domains=2 datamodule.dataset.z_dim=2,8,16,32,64 ~callbacks.visualization_callback model.penalty_weight=1.0,0.1,0.01 model.wait_steps=0,1000 model.linear_steps=1,2000 logger.wandb.tags=["mila","sweep-linGlinM-match-minmax-nohinge"] --multirun
+# num_domains=4
+# python run_training.py ckpt_path=null trainer.gpus=0 model/optimizer=adam,adamw model.optimizer.lr=0.05,0.01,0.001 model/scheduler_config=reduce_on_plateau model=mixing_synthetic datamodule=mixing datamodule.dataset.num_domains=4 datamodule.dataset.z_dim=2,8,16,32,64 ~callbacks.visualization_callback model.penalty_weight=1.0,0.1,0.01 model.wait_steps=0,1000 model.linear_steps=1,2000 logger.wandb.tags=["mila","sweep-linGlinM-match-minmax-nohinge"] --multirun
+# num_domains=8
+# python run_training.py ckpt_path=null trainer.gpus=0 model/optimizer=adam,adamw model.optimizer.lr=0.05,0.01,0.001 model/scheduler_config=reduce_on_plateau model=mixing_synthetic datamodule=mixing datamodule.dataset.num_domains=8 datamodule.dataset.z_dim=2,8,16,32,64 ~callbacks.visualization_callback model.penalty_weight=1.0,0.1,0.01 model.wait_steps=0,1000 model.linear_steps=1,2000 logger.wandb.tags=["mila","sweep-linGlinM-match-minmax-nohinge"] --multirun
+# num_domains=16
+# python run_training.py ckpt_path=null trainer.gpus=0 model/optimizer=adam,adamw model.optimizer.lr=0.05,0.01,0.001 model/scheduler_config=reduce_on_plateau model=mixing_synthetic datamodule=mixing datamodule.dataset.num_domains=16 datamodule.dataset.z_dim=2,8,16,32,64 ~callbacks.visualization_callback model.penalty_weight=1.0,0.1,0.01 model.wait_steps=0,1000 model.linear_steps=1,2000 logger.wandb.tags=["mila","sweep-linGlinM-match-minmax-nohinge"] --multirun
+
+# ------------------------------------------------------------------------------------- #
+# -------------- Sweep No dimension mismatch, std penalty, no hinge loss -------------- #
+# cpu, each sweep contains 2x4x2x2=32 runs
+# num_domains=2
+python run_training.py ckpt_path=null trainer.gpus=0 model/optimizer=adam model.penalty_criterion="stddev" model.hinge_loss_weight=0.0 model.optimizer.lr=0.005,0.001 model/scheduler_config=reduce_on_plateau model=mixing_synthetic datamodule=mixing datamodule.dataset.num_domains=2 datamodule.dataset.z_dim=8,16,32,64 ~callbacks.visualization_callback model.penalty_weight=1.0,0.1 model.linear_steps=1,2000 logger.wandb.tags=["mila","sweep-linGlinM-match-stddev-nohinge"] ~callbacks.early_stopping --multirun
+python run_training.py ckpt_path=null trainer.gpus=0 model/optimizer=adam model.penalty_criterion="stddev" model.hinge_loss_weight=0.0 model.optimizer.lr=0.005 model/scheduler_config=reduce_on_plateau model=mixing_synthetic datamodule=mixing datamodule.dataset.num_domains=2 datamodule.dataset.z_dim=8 ~callbacks.visualization_callback model.penalty_weight=1.0 model.linear_steps=1 logger.wandb.tags=["mila","test"] ~callbacks.early_stopping
+# num_domains=4
+# python run_training.py ckpt_path=null trainer.gpus=0 model/optimizer=adam,adamw model.optimizer.lr=0.05,0.01,0.001 model/scheduler_config=reduce_on_plateau model=mixing_synthetic datamodule=mixing datamodule.dataset.num_domains=4 datamodule.dataset.z_dim=2,8,16,32,64 ~callbacks.visualization_callback model.penalty_weight=1.0,0.1,0.01 model.wait_steps=0,1000 model.linear_steps=1,2000 logger.wandb.tags=["mila","sweep-linGlinM-match-minmax-nohinge"] ~callbacks.early_stopping --multirun
+# num_domains=8
+# python run_training.py ckpt_path=null trainer.gpus=0 model/optimizer=adam,adamw model.optimizer.lr=0.05,0.01,0.001 model/scheduler_config=reduce_on_plateau model=mixing_synthetic datamodule=mixing datamodule.dataset.num_domains=8 datamodule.dataset.z_dim=2,8,16,32,64 ~callbacks.visualization_callback model.penalty_weight=1.0,0.1,0.01 model.wait_steps=0,1000 model.linear_steps=1,2000 logger.wandb.tags=["mila","sweep-linGlinM-match-minmax-nohinge"] ~callbacks.early_stopping --multirun
+# num_domains=16
+# python run_training.py ckpt_path=null trainer.gpus=0 model/optimizer=adam,adamw model.optimizer.lr=0.05,0.01,0.001 model/scheduler_config=reduce_on_plateau model=mixing_synthetic datamodule=mixing datamodule.dataset.num_domains=16 datamodule.dataset.z_dim=2,8,16,32,64 ~callbacks.visualization_callback model.penalty_weight=1.0,0.1,0.01 model.wait_steps=0,1000 model.linear_steps=1,2000 logger.wandb.tags=["mila","sweep-linGlinM-match-minmax-nohinge"] ~callbacks.early_stopping --multirun
+
+
+
+# ------------------------------------------------------------------------------------- #
 # -------------------------- Linear Mixing, Non-Linear Model -------------------------- #
 # ------------------------------------------------------------------------------------- #
 
